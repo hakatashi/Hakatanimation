3abe34d89ef9894af6a4ebd367a9fa1213b63a04
hakatashi <hakatasiloving@gmail.com>
2013-07-19 17:28:14 +0900

    Remove comments

--- HakataAnime.cpp
+++ HakataAnime.cpp
@@ -1,30 +1,25 @@
 #include <windows.h>
 #include <tchar.h>
 
-// 定数
-#define WINDOW_WIDTH  (400)		// ウィンドウの幅
-#define WINDOW_HEIGHT (300)		// ウィンドウの高さ
+#define WINDOW_WIDTH 400
+#define WINDOW_HEIGHT 300
 #define WINDOW_X ((GetSystemMetrics( SM_CXSCREEN ) - WINDOW_WIDTH ) / 2)
 #define WINDOW_Y ((GetSystemMetrics( SM_CYSCREEN ) - WINDOW_HEIGHT ) / 2)
-#define TIMER_ID     (100)      // 作成するタイマの識別ID
-#define TIMER_ELAPSE (30)     // WM_TIMERの発生間隔
-#define WM_TASKTRAY (WM_USER+1)
+#define TIMER_ID 100
+#define TIMER_ELAPSE 30
+#define WM_TASKTRAY WM_USER+1
 #define ID_TASKTRAY 0
-#define ID_DUMMY   0
-#define ID_EXIT    1
+#define ID_DUMMY 0
+#define ID_EXIT 1
 
-// プロトタイプ宣言
 HWND Create(HINSTANCE hInst);
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
 
-
-// 開始位置
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR pCmdLine, int showCmd)
 {
 	HWND hWnd;
 	MSG msg;
 
-	// ウィンドウを作成する
 	hWnd = Create( hInst );
 	if( hWnd == NULL )
 	{
@@ -32,81 +27,66 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR pCmdLine, int sho
 		return 1;
 	}
 
-	// ウィンドウを表示する
 	ShowWindow( hWnd, SW_SHOW );
 	UpdateWindow( hWnd );
 
-	// タイマを作成する
 	SetTimer( hWnd, TIMER_ID, TIMER_ELAPSE, NULL );
 
-	// メッセージループ
 	while( 1 )
 	{
-		BOOL ret = GetMessage( &msg, NULL, 0, 0 );  // メッセージを取得する
+		BOOL ret = GetMessage( &msg, NULL, 0, 0 ); 
 		if( ret == 0 || ret == -1 )
 		{
-			// アプリケーションを終了させるメッセージが来ていたら、
-			// あるいは GetMessage() が失敗したら( -1 が返されたら ）、ループを抜ける
 			break;
 		}
 		else
 		{
-			// メッセージを処理する
 			TranslateMessage( &msg );
 			DispatchMessage( &msg );
 		}
 	}
 
-	// タイマを破棄する
 	KillTimer( hWnd, TIMER_ID );
 
 	return 0;
 }
 
-// ウィンドウを作成する
 HWND Create(HINSTANCE hInst)
 {
 	WNDCLASSEX wc;
 	HWND g_hWnd;
 
-	// ウィンドウクラスの情報を設定
-	wc.cbSize = sizeof(wc);               // 構造体サイズ
-	wc.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;   // スタイル
-	wc.lpfnWndProc = WndProc;             // ウィンドウプロシージャ
-	wc.cbClsExtra = 0;                    // 拡張情報１
-	wc.cbWndExtra = 0;                    // 拡張情報２
-	wc.hInstance = hInst;                 // インスタンスハンドル
-	wc.hIcon = LoadIcon(hInst , TEXT("KITTY"));          // アイコン
-	wc.hIconSm = wc.hIcon;                // 子アイコン
-	wc.hCursor = (HCURSOR)LoadImage(      // マウスカーソル
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInst;
+	wc.hIcon = LoadIcon(hInst , TEXT("KITTY"));
+	wc.hIconSm = wc.hIcon;
+	wc.hCursor = (HCURSOR)LoadImage(
 		NULL, MAKEINTRESOURCE(IDC_ARROW), IMAGE_CURSOR,
 		0, 0, LR_DEFAULTSIZE | LR_SHARED
 	);
-	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH); // ウィンドウ背景
-	wc.lpszMenuName = NULL;                     // メニュー名
-	wc.lpszClassName = _T("Default Class Name");// ウィンドウクラス名
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = _T("Default Class Name");
 	
-	// ウィンドウクラスを登録する
 	if( RegisterClassEx( &wc ) == 0 ){ return NULL; }
 
-	// ウィンドウを作成する
 	g_hWnd = CreateWindowEx(
 		WS_EX_TOOLWINDOW,
-		wc.lpszClassName,      // ウィンドウクラス名
-		_T("Sample Program"),  // タイトルバーに表示する文字列
-		WS_OVERLAPPEDWINDOW,   // ウィンドウの種類
-		0,              // ウィンドウを表示する位置（X座標）
-		0,              // ウィンドウを表示する位置（Y座標）
-		0,          // ウィンドウの幅
-		0,         // ウィンドウの高さ
-		NULL,                  // 親ウィンドウのウィンドウハンドル
-		NULL,                  // メニューハンドル
-		hInst,                 // インスタンスハンドル
-		NULL                   // その他の作成データ
+		wc.lpszClassName,
+		_T("Sample Program"),
+		WS_OVERLAPPEDWINDOW,
+		0, 0, 0, 0,
+		NULL,
+		NULL,
+		hInst,
+		NULL
 	);
 	
 	NOTIFYICONDATA nif;
-	// タスクトレイに登録
 	nif.cbSize				= sizeof( NOTIFYICONDATA );
 	nif.hIcon				= LoadIcon(hInst , TEXT("KITTY"));
 	nif.hWnd				= g_hWnd;
@@ -119,8 +99,7 @@ HWND Create(HINSTANCE hInst)
 	
 	return g_hWnd;
 }
-
-// ウィンドウプロシージャ
+	
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 {
 	HDC hDC;
@@ -130,50 +109,45 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 
 	switch( msg )
 	{
-	case WM_CREATE:         // ウィンドウが作られたとき
-		// メニューリソースをロードする
+	case WM_CREATE:
 		hMenu = LoadMenu( NULL, _T("IDR_POPUP") );
-		// サブメニューのハンドルを取得する
 		hSubMenu = GetSubMenu( hMenu, 0 );
 
 		return 0;
 	
-	case WM_TIMER:          // タイマメッセージ
+	case WM_TIMER:
 		if( wp != TIMER_ID )
 		{
-			break;  // 識別IDが一致しないタイマメッセージはDefWindowProc()に任せる
+			break;
 		}
-
-		//InvalidateRect( hWnd, NULL, FALSE );
 		return 0;
 
-	case WM_PAINT:          // 無効領域があるとき
+	case WM_PAINT:
 		return 0;
 
-	case WM_TASKTRAY: // カーソルの現在位置を取得
+	case WM_TASKTRAY:
 		POINT point;
 		GetCursorPos(&point);
 
 		switch (lp) {
 		case WM_RBUTTONDOWN:
-			SetForegroundWindow(hWnd);	// この処理を忘れずに！
+			SetForegroundWindow(hWnd);
 			
 			TrackPopupMenu( hSubMenu, TPM_LEFTALIGN | TPM_BOTTOMALIGN, point.x, point.y, 0, hWnd, NULL );
-			//PostMessage(WM_NULL);	// この処理も忘れずに！
 			break;
 		}
 		return 0;
 
-	case WM_COMMAND:        // 項目が選択されたとき
+	case WM_COMMAND:
 		switch( LOWORD( wp ) )
 		{
-		case ID_EXIT:       // 「終了」
+		case ID_EXIT:
 			SendMessage( hWnd, WM_CLOSE, 0, 0 );
 			break;
 		}
 		return 0;
 
-	case WM_DESTROY:        // ウィンドウが破棄されるとき
+	case WM_DESTROY:
 		PostQuitMessage( 0 );
 		return 0;
 	}
